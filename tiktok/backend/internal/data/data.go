143aa9d0f8dca00408c1da641db839ca69259aee
ShowyQuasar88 <ginwithouta@gmail.com>
2024-11-21 14:37:17 +0800

    feature: init config of postgres with gorm

--- tiktok/backend/internal/data/data.go
+++ tiktok/backend/internal/data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"backend/internal/conf"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
@@ -12,13 +14,41 @@ var ProviderSet = wire.NewSet(NewData, NewGreeterRepo, NewUserRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db  *gorm.DB
+	log *log.Helper
 }
 
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
+	log := log.NewHelper(logger)
+
+	db, err := gorm.Open(postgres.Open(c.Database.Source), &gorm.Config{})
+	if err != nil {
+		log.Errorf("failed opening connection to postgres: %v", err)
+		return nil, nil, err
 	}
-	return &Data{}, cleanup, nil
+
+	// 获取通用数据库对象 sql.DB,然后使用其提供的功能
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Errorf("failed to get db instance: %v", err)
+		return nil, nil, err
+	}
+
+	// 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+	// 设置打开数据库连接的最大数量
+	sqlDB.SetMaxOpenConns(100)
+
+	cleanup := func() {
+		log.Info("closing database resources")
+		if err := sqlDB.Close(); err != nil {
+			log.Errorf("failed to close database resources: %v", err)
+		}
+	}
+
+	return &Data{
+		db:  db,
+		log: log,
+	}, cleanup, nil
 }
