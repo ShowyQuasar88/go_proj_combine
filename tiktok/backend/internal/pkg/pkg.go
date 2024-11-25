33436dc8b3fe023462ad3c6813765f41aff60cc5
ShowyQuasar88 <ginwithouta@gmail.com>
2024-11-25 17:28:48 +0800

    feature: add jwt wire

--- tiktok/backend/internal/pkg/pkg.go
+++ tiktok/backend/internal/pkg/pkg.go
@@ -1,9 +1,14 @@
 package pkg
 
 import (
+	"backend/internal/pkg/auth"
 	"backend/internal/pkg/crypto"
 	"github.com/google/wire"
 )
 
 // ProviderSet 提供基础设施层的依赖
-var ProviderSet = wire.NewSet(crypto.NewCrypto)
+var ProviderSet = wire.NewSet(
+	crypto.NewCrypto,
+	auth.NewJWTHelper,
+	auth.NewJWTConfig,
+)
