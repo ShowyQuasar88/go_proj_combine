dcbb05a4301a00840f1c37f7dc2dca13e6023852
ShowyQuasar88 <ginwithouta@gmail.com>
2024-12-18 17:29:02 +0800

    classical 150: PB 027 Two sum II input array is sorted

---
+++ algorithm/classical_150/PB_027_TwoSumIIInputArrayIsSorted.go
@@ -0,0 +1,16 @@
+package classical_150
+
+// twoSum 两数之和 II - 输入有序数组
+func twoSum(numbers []int, target int) []int {
+	left, right := 0, len(numbers)-1
+	for left < right {
+		if numbers[left]+numbers[right] > target {
+			right--
+		} else if numbers[left]+numbers[right] < target {
+			left++
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+	return []int{0, 0}
+}
