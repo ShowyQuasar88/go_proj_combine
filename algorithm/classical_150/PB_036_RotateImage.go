aaffa3f57f2e40b1456555335a52b341273107d2
ShowyQuasar88 <ginwithouta@gmail.com>
2024-12-20 09:19:49 +0800

    classical 150: PB 036 Rotate image

---
+++ algorithm/classical_150/PB_036_RotateImage.go
@@ -0,0 +1,14 @@
+package classical_150
+
+// rotateImage 旋转图像
+func RotateImage(matrix [][]int) {
+	for top, bottom := 0, len(matrix)-1; top < bottom; top, bottom = top+1, bottom-1 {
+		for begin := 0; top+begin < bottom; begin++ {
+			tmp := matrix[top][top+begin]
+			matrix[top][top+begin] = matrix[bottom-begin][top]
+			matrix[bottom-begin][top] = matrix[bottom][bottom-begin]
+			matrix[bottom][bottom-begin] = matrix[top+begin][bottom]
+			matrix[top+begin][bottom] = tmp
+		}
+	}
+}
